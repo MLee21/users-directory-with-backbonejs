d205c7ab243107330bc36a481ed4c64f2ec18955
MLee21 <minnie.lee89@gmail.com>
2016-03-08 21:59:07 -0700

    set up view: element, initialize view when created, render the view template from model data. create a new instance of collection. output the objects inside of a render function on each iteration over the collection.

--- js/app.js
+++ js/app.js
@@ -16,11 +16,35 @@
     url: "assets/users.json"
   });
 
-  var testCollection = new UsersCollection();
-  testCollection.fetch({
-    success: function() {
-      console.log(testCollection);
+  var UsersView = Backbone.View.extend({
+    el: "#users__body",
+    collection: new UsersCollection(),
+    initialize: function() {
+      var scope = this;
+      this.collection.fetch({
+        success: function() {
+          scope.render();
+        }
+      });
+    },
+
+    render: function() {
+      var scope = this;
+      this.collection.forEach(function(model) {
+        scope.output(model);
+      });
+      return this;
+    }, 
+
+    output: function(model) {
+      var row = document.createElement('tr');
+      row.innerHTML = "<td>" + model.get("first name") + "</td>\
+                       <td>" + model.get("last name") + "</td>\
+                       <td>" + model.get("email") + "</td>";
+      this.el.appendChild(row);
     }
   });
+
+  var app = new UsersView();
 
 })();
