73f796fb7a8c94cfc613caf965d23ebb272314ed
MLee21 <minnie.lee89@gmail.com>
2016-03-08 21:43:01 -0700

    establish and setup model.

---
+++ js/app.js
@@ -0,0 +1,14 @@
+(function() {
+
+  var UsersModel = Backbone.Model.extend({
+    defaults: function() {
+      return {
+        firstname: "",
+        lastname: "",
+        email: ""
+      }
+    }
+  });
+
+
+})();
